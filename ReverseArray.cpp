dbba479d69ddb41ca730e42a3629f49dbfe088ef
Abu Bakar Siddique <111673862+siddique0203@users.noreply.github.com>
2023-06-14 21:45:10 +0600

    ReverseArray.cpp

--- ReverseArray.cpp
+++ ReverseArray.cpp
@@ -1,3 +1,13 @@
+ /*
+ Initialize TWO integer arrays of different sizes. Merge the input arrays and create a new array. Then print the new array in reverse order.
+
+  For example,
+  Array_1 = {10,20,30,40,50}
+  Array_2 = {1,2,3,4,5,6,7,8}
+
+  Output: 8 7 6 5 4 3 2 1 50 40 30 20 10
+/*
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -23,18 +33,5 @@ int main(){
     for(int i=0; i<total; i++){
         cout<<arr2[i]<<" ";
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
