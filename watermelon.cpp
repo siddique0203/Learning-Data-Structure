c43384db0410d928e8acd078d2d29dac89e72900
Abu Bakar Siddique <111673862+siddique0203@users.noreply.github.com>
2023-06-15 19:26:30 +0600

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a;
+cout<<"enter weight : ";
+cin>>a;
+      if(1<=a && a<=100){
+
+        if(a%2==0)
+          {cout<<"YES"<<endl;}
+          else
+         {cout<<"NO"<<endl;}
+
+  }
+}
+
+
