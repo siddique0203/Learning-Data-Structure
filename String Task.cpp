03bf6a0dc77684d713ce6658066a67d7ab9ebf27
Abu Bakar Siddique <111673862+siddique0203@users.noreply.github.com>
2023-06-30 22:33:23 +0600

    Update String Task.cpp

--- String Task.cpp
+++ String Task.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 
 int main() {
-    char s[100];
+    char s[100];  
 
     cin >> s;
     int length = strlen(s);
 
-    for(int i=0; i<length; i++){
-      s[i]=(char)tolower(s[i]);
+    for (int i = 0; i < length; i++) {
+        s[i] = tolower(s[i]);
     }
 
     for (int i = 0; i < length; i++) {
-         if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i]=='y') {
             continue;
-         }
-         else{
-            cout<<"."<<s[i];
-         }
+        } else {
+            cout << '.' << s[i];
+        }
     }
 
-
+     
 }
