66fba40ef95d1e9fd491ae898850bbaf45cf661b
Abu Bakar Siddique <111673862+siddique0203@users.noreply.github.com>
2023-07-02 17:19:10 +0600

    Add files via upload

---
+++ Bear and Big Brother.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,count=0;
+    cin>>a>>b;
+    if(a>b)
+        count=0;
+    else
+        while(a<=b)
+        {
+            a=a*3;
+            b=b*2;
+            count++;
+        }
+    cout<<count<<endl;
+
+}
